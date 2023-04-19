cac2e01e3df6b822ed2312e743cea80ad7df8738
Xavimont0409 <xavier.mont.0409@gmail.com>
2023-04-19 15:00:58 -0500

    logica terminada

--- client/src/components/FormPage.jsx
+++ client/src/components/FormPage.jsx
@@ -1,7 +1,7 @@
 import "../styles/FormPage.css";
 import { validate } from "../validadoForm/validate";
 import { useState, useEffect } from "react";
-import { getAllTypes } from "../redux/actions/actions";
+import { getAllTypes, postPokemon } from "../redux/actions/actions";
 import { useSelector, useDispatch } from "react-redux";
 
 const FormPage = () => {
@@ -42,9 +42,25 @@ const FormPage = () => {
     setError(validate({ ...creatNewPokemon, [property]: value }));
   };
 
+  const handleTypeChange = (event) =>{
+    const value = event.target.value
+    if(!creatNewPokemon.types.includes(value)){
+      setCreateNewPokemon({...creatNewPokemon, types:[...creatNewPokemon.types, value]  })
+    }
+  }
+
+  const handleOnSubmit = () =>{
+    if(creatNewPokemon.name && creatNewPokemon.image && creatNewPokemon.life && creatNewPokemon.attack && creatNewPokemon.types){
+      dispatch(postPokemon(creatNewPokemon));
+    }else{
+      alert("falta completar datos") 
+    }
+
+  }
+
   return (
     <div className="container-form">
-      <form action="">
+      <form >
         <h3>Create Pokemon</h3>
         <div>
           <label htmlFor="name">Nombre</label>
@@ -54,42 +70,53 @@ const FormPage = () => {
         <div>
           <label htmlFor="image">Link Imagen</label>
           <input type="text" name="image" value={creatNewPokemon.image} onChange={handleInputChange} />
+          <p>{error.image}</p>
         </div>
         <div>
           <label htmlFor="life">Vida</label>
           <input type="text" name="life" value={creatNewPokemon.life} onChange={handleInputChange} />
+          <p>{error.life}</p>
         </div>
         <div>
           <label htmlFor="attack">Ataque</label>
           <input type="text" name="attack" value={creatNewPokemon.attack} onChange={handleInputChange} />
+          <p>{error.attack}</p>
         </div>
         <div>
           <label htmlFor="defense">Defensa</label>
           <input type="text" name="defense" value={creatNewPokemon.defense} onChange={handleInputChange} />
+          <p>{error.defense}</p>
         </div>
         <div>
           <label htmlFor="speed">Velocidad</label>
           <input type="text" name="speed" value={creatNewPokemon.speed} onChange={handleInputChange} />
+          <p>{error.speed}</p>
         </div>
         <div>
-          <label htmlFor="heigh">Altura</label>
-          <input type="text" name="heigh" value={creatNewPokemon.height} onChange={handleInputChange} />
+          <label htmlFor="height">Altura</label>
+          <input type="text" name="height" value={creatNewPokemon.height} onChange={handleInputChange} />
+          <p>{error.height}</p>
         </div>
         <div>
           <label htmlFor="weight">Peso</label>
           <input type="text" name="weight" value={creatNewPokemon.weight} onChange={handleInputChange} />
+          <p>{error.weight}</p>
         </div>
         <label>
-          Por Tipo -
-          <select>
+          Por Tipo - 
+          <select onChange={(event)=> handleTypeChange(event)}>
             <option value="-">-</option>
             {allTypes.map((type) => (
-              <option value={type.name} key={type.id}>
+              <option value={type.name} id={type.id} key={type.id}>
                 {type.name}
               </option>
             ))}
           </select>
         </label>
+          {
+            creatNewPokemon.types.length > 0 && creatNewPokemon.types.map((type)=><span key={type}>*{type}</span> ) 
+          }
+          <button onClick={()=>handleOnSubmit()} >AGREGAR</button>
       </form>
     </div>
   );
--- client/src/validadoForm/validate.js
+++ client/src/validadoForm/validate.js
@@ -1,11 +1,53 @@
-export const validate = (creatNewPokemon) =>{ 
-let regexName =  /^[a-z]+$/
+export const validate = (creatNewPokemon) => {
+  let regexName = /^[a-z]+$/;
+  let regexImg =
+    /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/;
+  let rexNum = /^([0-9])*$/;  
 
-  let errors = {}
+  let errors = {};
   //! VALIDACION NAME
-  if(!regexName.test(creatNewPokemon.name) ) errors.name ="Nombre no puede tener numero"
-  if(!creatNewPokemon.name) errors.name = "El nombre de usuario no debe estar vacio";
-  if(creatNewPokemon.name !== creatNewPokemon.name.toLowerCase()) errors.name ="El nombre no puede tener mayusculas";
+  if (!creatNewPokemon.name)
+    errors.name = "El nombre de usuario no debe estar vacio";
+  else if (!regexName.test(creatNewPokemon.name))
+    errors.name = "Nombre no puede tener numero";
+  else if (creatNewPokemon.name !== creatNewPokemon.name.toLowerCase())
+    errors.name = "El nombre no puede tener mayusculas";
 
-  return errors
-}
+  //!Validacion link Image
+  if (creatNewPokemon.image) {
+    if (!regexImg.test(creatNewPokemon.image))
+      errors.image = "Tiene que ser un Link de Imagen";
+  }
+
+  //! validacion vida
+  if(creatNewPokemon.life){
+    if(!rexNum.test(creatNewPokemon.life)) errors.life= "Solo numeros"
+  }
+
+  //!validacion attack
+  if(creatNewPokemon.attack){
+    if(!rexNum.test(creatNewPokemon.attack)) errors.attack= "Solo numeros"
+  }
+
+  //!validacion defense
+  if(creatNewPokemon.defense){
+    if(!rexNum.test(creatNewPokemon.defense)) errors.defense = "solo numeros"
+  }  
+
+  //!validacion spped
+    if(creatNewPokemon.speed){
+      if(!rexNum.test(creatNewPokemon.speed)) errors.speed = "solo numeros"
+    }  
+
+  //!validacion height
+  if(creatNewPokemon.height){
+    if(!rexNum.test(creatNewPokemon.height)) errors.height = "solo numeros"
+  }  
+
+  //!validacion weight
+  if(creatNewPokemon.weight){
+    if(!rexNum.test(creatNewPokemon.weight)) errors.weight= "solo numeros"
+  }
+
+  return errors;
+};
