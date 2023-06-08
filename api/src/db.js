121a68ae05e73a182c627d102b019d0e50e314b2
Xavimont0409 <111645686+Xaviermon@users.noreply.github.com>
2023-06-08 18:35:12 -0500

    deploy bd

--- api/src/db.js
+++ api/src/db.js
@@ -2,11 +2,17 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY } = process.env;
 //const  PokemonFuntion = require('./models/Pokemon')
 //const  TypesFuntion = require('./models/types')
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`,
+/* const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`,
+  {
+    logging: false, // set to console.log to see the raw SQL queries
+    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  }
+); */
+const sequelize = new Sequelize(DB_DEPLOY,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
--- client/src/components/views/LandingPage/LandingPage.jsx
+++ client/src/components/views/LandingPage/LandingPage.jsx
@@ -6,18 +6,6 @@ const LandingPage = () => {
   return (
     <div className="container">
       <div className="capsula">
-        <div className="div-frase">
-          <p>
-            Welcome to PokeXavi! my Pokémon themed website for the Individual
-            Project stage! I hope you find everything you are looking for. At
-            this website, you will find a lot of information about the world of
-            Pokémon. You will also be able to create your own Pokémon teams,
-            train them, and battle with other trainers from the community. Thank
-            you for visiting us, we hope you enjoy browsing our website! Let's
-            get started, choose your starter Pokémon and let's begin our journey
-            together. Made by Xavier Montero with ❤️.
-          </p>
-        </div>
         <div className="div-img">
           <Link to="/home">
             <img className="start-button" src={start} alt="" />
